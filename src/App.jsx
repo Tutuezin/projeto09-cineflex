6dd3d21a773c0b584b5821136de9eea9ad2fb0a7
Tutuezin <vinnycamara1@gmail.com>
2022-05-17 15:39:18 -0300

    :tada: Inicio do Projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GlobalStyle from "./assets/css/GlobalStyle";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  );
+}
+
+export default App;
